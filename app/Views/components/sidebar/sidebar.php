065f901497d3d7883e947569c9e42d472bda7289
Lucas Acosta <lucas.acosta@redtecnologica.org>
2024-01-24 21:50:55 -0300

    challenge terminado

--- app/Views/components/sidebar/sidebar.php
+++ app/Views/components/sidebar/sidebar.php
@@ -1,5 +1,5 @@
 <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+    <a href="/producto" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
         <span class="fs-4">Challenge</span>
     </a>
     <hr>
