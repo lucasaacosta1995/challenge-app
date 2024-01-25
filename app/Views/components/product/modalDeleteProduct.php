bc84019533611ab0c77693be525e6ecca3f8907b
Lucas Acosta <lucas.acosta@redtecnologica.org>
2024-01-24 21:48:36 -0300

    challenge terminado

--- app/Views/components/product/modalDeleteProduct.php
+++ app/Views/components/product/modalDeleteProduct.php
@@ -3,7 +3,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="deleteModalLabel">Confirmar Borrado</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Cerrar">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
